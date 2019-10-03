42d622f595f01f58191599c75380b9f5f93aa223
vneekhra <vikasnikhra@gmail.com>
2019-10-02 19:56:18 -0600

    updated app.js file

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const http = require("http");
 
-const hostname = "127.0.0.1";
+//const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/plain');
-    res.end('Hello World');
+    res.end('Hello World... this is node js');
 });
 
-server.listen(port, hostname, () => {
+//server.listen(port, hostname, () => {
+server.listen(port, () => {
     console.log('Server started on port '+ port);
 });
